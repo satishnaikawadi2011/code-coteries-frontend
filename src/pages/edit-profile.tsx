51b2ef044dd6c27ac22ad679dd7cc63842042bd9
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-10-26 13:23:32 +0530

    Added 8 pages with boilerplate code.

---
+++ src/pages/edit-profile.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditProfilePage = () => {
+	return <h1>Edit Profile Page</h1>;
+};
+
+export default EditProfilePage;
---
+++ src/pages/explore.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExplorePage = () => {
+	return <h1>Explore Page</h1>;
+};
+
+export default ExplorePage;
