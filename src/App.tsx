192d07d171e48148823ad8a54355a460c79445dc
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-10-26 13:31:50 +0530

    Setup routes for all added pages with react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
-import { useGetAllPostsQuery } from './generated/graphql';
-import LogoIcon from './icons/LogoIcon';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import FeedPage from './pages/feed';
+import NotFoundPage from './pages/not-found';
+import ExplorePage from './pages/explore';
+import ProfilePage from './pages/profile';
+import PostPage from './pages/post';
+import EditProfilePage from './pages/edit-profile';
+import LoginPage from './pages/login';
+import SignupPage from './pages/signup';
 
 function App() {
-	const { loading, data } = useGetAllPostsQuery();
-	console.log(data);
-	if (loading) {
-		return <h1>Loading ....</h1>;
-	}
-	return <h1 className="title">Code Coteries</h1>;
-	// return <LogoIcon width={50} height={50} />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={FeedPage} />
+				<Route exact path="/explore" component={ExplorePage} />
+				<Route exact path="/accounts/edit" component={EditProfilePage} />
+				<Route exact path="/accounts/login" component={LoginPage} />
+				<Route exact path="/accounts/signup" component={SignupPage} />
+				<Route exact path="/p/:postId" component={PostPage} />
+				<Route exact path="/:username" component={ProfilePage} />
+				<Route path="*" component={NotFoundPage} />
+			</Switch>
+		</Router>
+	);
 }
 export default App;
