49d5e8b9a9f02dcde87e238fcfd2f91c225bc7a9
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-10-27 13:17:53 +0530

    Successfully added language and theme types and key-value paired objects in according to carbon.now.sh app.

---
+++ src/types/theme.ts
@@ -0,0 +1,58 @@
+export type themeName =
+	| 'a11y-dark'
+	| 'blackboard'
+	| 'base16-dark'
+	| 'base16-light'
+	| 'cobalt'
+	| 'duotone-dark'
+	| 'hopscotch'
+	| 'lucario'
+	| 'material'
+	| 'monokai'
+	| 'night-owl'
+	| 'nord'
+	| 'oceanic-next'
+	| 'one-light'
+	| 'one-dark'
+	| 'panda-syntax'
+	| 'paraiso-dark'
+	| 'seti'
+	| 'shades-of-purple'
+	| 'solarized+dark'
+	| 'solarized+light'
+	| 'synthwave-84'
+	| 'twilight'
+	| 'verminal'
+	| 'vscode'
+	| 'yeti'
+	| 'zenburn';
+
+export const themes: Record<themeName, string> = {
+	'a11y-dark': 'A11y Dark',
+	blackboard: 'Blackboard',
+	'base16-dark': 'Base 16 (Dark)',
+	'base16-light': 'Base 16 (Light)',
+	cobalt: 'Cobalt',
+	'duotone-dark': 'Duotone',
+	hopscotch: 'Hopscotch',
+	lucario: 'Lucario',
+	material: 'Material',
+	monokai: 'Monokai',
+	'night-owl': 'Night Owl',
+	nord: 'Nord',
+	'oceanic-next': 'Oceanic Next',
+	'one-dark': 'One Dark',
+	'one-light': 'One Light',
+	'panda-syntax': 'Panda',
+	'paraiso-dark': 'Paraiso',
+	seti: 'Seti',
+	'shades-of-purple': 'Shades Of Purple',
+	'solarized+dark': 'Solarized Dark',
+	'solarized+light': 'Solarized Light',
+	'synthwave-84': 'SynthWave 84',
+	twilight: 'Twilight',
+	verminal: 'Verminal',
+	vscode: 'VSCode',
+	yeti: 'Yeti',
+	zenburn: 'Zenburn'
+};
